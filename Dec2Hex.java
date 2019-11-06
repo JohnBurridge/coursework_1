f759de537c7612e6c155c9c65144fde9e0479efb
JBurridge <JOHNBURRI200@caledonian.ac.uk>
2019-11-06 13:43:32 +0000

    commit tidy

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -23,10 +23,11 @@ class Dec2Hex
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
-	String hexadecimal=""; 
+	String hexadecimal="";
+ 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-        while(num != 0)
+        while(num > 0)
         {
 	    rem=num%16;
 	    hexadecimal= ch[rem] + hexadecimal;
